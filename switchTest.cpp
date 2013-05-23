b60858ebce8f3dc56ab32faf95cd48e049be05d6
Aaron Frantisak <afrantisak@gmail.com>
2013-05-23 18:24:11 -0500

    get the cooler wrapper working

--- switchTest.cpp
+++ switchTest.cpp
@@ -1,38 +1,42 @@
 #include <iostream>
 #include <functional>
+#include <memory>
 #include <map>
 
 template<typename Key, typename Func = std::function<void()>>
-class Switch
+class Switcher
 {
     public:
-        Switch()
+        Switcher()
             :   m_impl(),
                 m_default()
         {
         }
-
-        void Case(Key key, Func func)
+        
+        Switcher& Case(Key key, Func func)
         {
             m_impl.insert(std::make_pair(key, func));
+            return *this;
         }
 
-        void Default(Func func)
+        Switcher& Default(Func func)
         {
             m_default = func;
+            return *this;
         }
 
-        void operator()(Key key)
+        Switcher& operator()(Key key)
         {
             auto iFunc = m_impl.find(key);
-            if (iFunc == m_impl.end())
-            {
-                m_default();
-            }
-            else
+            if (iFunc != m_impl.end())
             {
                 iFunc->second();
             }
+            else
+            {
+                m_default();
+            }
+            return *this;
         }
 
     private:
@@ -40,10 +44,90 @@ class Switch
         Impl m_impl;
         Func m_default;
 };
+
+template<typename Key, typename Func = std::function<void()>>
+class AutoSwitcher
+{
+    public:
+        AutoSwitcher(Key key)
+            :   m_impl(),
+                m_key(key)
+        {
+        }
+        
+        ~AutoSwitcher()
+        {
+            m_impl(m_key);
+        }
+        
+        AutoSwitcher& Case(Key key, Func func)
+        {
+            m_impl.Case(key, func);
+            return *this;
+        }
+
+        AutoSwitcher& Default(Func func)
+        {
+            m_impl.Default(func);
+            return *this;
+        }
+
+    private:
+        Switcher<Key, Func> m_impl;
+        Key m_key;
+};
+
+template<typename Key, typename Func = std::function<void()>>
+class AutoSwitcherWrap
+{
+    public:
+        AutoSwitcherWrap(Key key)
+            :   m_pImpl(new AutoSwitcher<Key, Func>(key))
+        {
+        }
+        
+        AutoSwitcherWrap& Case(Key key, Func func)
+        {
+            m_pImpl->Case(key, func);
+            return *this;
+        }
+
+        AutoSwitcherWrap& Default(Func func)
+        {
+            m_pImpl->Default(func);
+            return *this;
+        }
+
+    private:
+        std::shared_ptr<AutoSwitcher<Key, Func>> m_pImpl;
+};
+
+template<typename Key, typename Func = std::function<void()>>
+AutoSwitcherWrap<Key, Func> Switch(Key key)
+{
+    return AutoSwitcherWrap<Key, Func>(key);
+}
 		
 int main(int argc, char* argv[])
 {
-    Switch<std::string> s;
+    std::cout << "new: " << std::endl;
+    
+    Switch(std::string(argv[1])).
+    Case(std::string("a"), []()
+    {
+        std::cout << "A" << std::endl; 
+    }).
+    Case(std::string("b"), []()
+    {
+        std::cout << "B" << std::endl; 
+    }).
+    Default([]()
+    {
+        std::cout << "default" << std::endl; 
+    });
+    
+    std::cout << "old: " << std::endl;
+    Switcher<std::string> s;
     s.Case("a", []()
     {
         std::cout << "A" << std::endl; 
@@ -57,5 +141,6 @@ int main(int argc, char* argv[])
         std::cout << "default" << std::endl; 
     });
     s(argv[1]);
+
     return 0;
 }
