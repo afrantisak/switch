9bc0f39aa8dd323f09a41b6db3a251555b07845e
Aaron Frantisak <afrantisak@gmail.com>
2013-05-23 19:46:06 -0500

    hide implementation

--- switch.h
+++ switch.h
@@ -1,80 +1,85 @@
-#include <iostream>
+#pragma once
 #include <functional>
 #include <memory>
 #include <map>
 
-template<typename Key>
-class Switcher
+namespace Private
 {
-    public:
-        typedef std::function<void()> Func;
-        
-        Switcher(Key key)
-            :   m_impl(),
-                m_default(),
-                m_key(key)
-        {
-        }
-        
-        void Case(Key key, Func func)
-        {
-            m_impl.insert(std::make_pair(key, func));
-        }
 
-        void Default(Func func)
-        {
-            m_default = func;
-        }
-
-        ~Switcher()
-        {
-            auto iFunc = m_impl.find(m_key);
-            if (iFunc != m_impl.end())
+    template<typename Key>
+    class SwitchImpl
+    {
+        public:
+            typedef std::function<void()> Func;
+            
+            SwitchImpl(Key key)
+                :   m_impl(),
+                    m_default(),
+                    m_key(key)
             {
-                iFunc->second();
             }
-            else
+            
+            void Case(Key key, Func func)
             {
-                m_default();
+                m_impl.insert(std::make_pair(key, func));
             }
-        }
 
-    private:
-        typedef std::map<Key, Func> Impl;
-        Impl m_impl;
-        Func m_default;
-        Key m_key;
-};
+            void Default(Func func)
+            {
+                m_default = func;
+            }
+
+            ~SwitchImpl()
+            {
+                auto iFunc = m_impl.find(m_key);
+                if (iFunc != m_impl.end())
+                {
+                    iFunc->second();
+                }
+                else
+                {
+                    m_default();
+                }
+            }
+
+        private:
+            typedef std::map<Key, Func> Impl;
+            Impl m_impl;
+            Func m_default;
+            Key m_key;
+    };
+
+    template<typename Key>
+    class SwitchWrap
+    {
+        public:
+            typedef typename SwitchImpl<Key>::Func Func;
+            
+            SwitchWrap(Key key)
+                :   m_pImpl(new SwitchImpl<Key>(key))
+            {
+            }
+            
+            SwitchWrap& Case(Key key, Func func)
+            {
+                m_pImpl->Case(key, func);
+                return *this;
+            }
+
+            SwitchWrap& Default(Func func)
+            {
+                m_pImpl->Default(func);
+                return *this;
+            }
+
+        private:
+            std::unique_ptr<SwitchImpl<Key>> m_pImpl;
+    };
+
+};// namespace Private
 
 template<typename Key>
-class SwitcherWrap
+Private::SwitchWrap<Key> Switch(Key key)
 {
-    public:
-        typedef typename Switcher<Key>::Func Func;
-        
-        SwitcherWrap(Key key)
-            :   m_pImpl(new Switcher<Key>(key))
-        {
-        }
-        
-        SwitcherWrap& Case(Key key, Func func)
-        {
-            m_pImpl->Case(key, func);
-            return *this;
-        }
-
-        SwitcherWrap& Default(Func func)
-        {
-            m_pImpl->Default(func);
-            return *this;
-        }
-
-    private:
-        std::unique_ptr<Switcher<Key>> m_pImpl;
-};
-
-template<typename Key>
-SwitcherWrap<Key> Switch(Key key)
-{
-    return SwitcherWrap<Key>(key);
+    return Private::SwitchWrap<Key>(key);
 }
